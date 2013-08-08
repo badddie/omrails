f30493c5a22bbefc93b09416e590b3b24d7776d3
baddie <midra.tech@gmail.com>
2013-08-08 11:18:41 +0200

    initial commit

---
+++ app/models/pin.rb
@@ -0,0 +1,8 @@
+class Pin < ActiveRecord::Base
+  attr_accessible :description, :image
+
+  validates :description, presence: true
+  validates :user_id, presence: true
+  belongs_to :user
+  has_attached_file :image, styles: {medium: "320x240>"}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerOsxDemo::Application.config.secret_token = '3fea8605a030985e609ea04f562756446a16af5ef3d2c28b81ec76d91999a080193f7d7dd72af825a693bd866212469a7c5128b83074581d4799f3d257e32452'
